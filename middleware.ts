3eb78bfa922f0e68ba57f2994b95d369b71c58e6
fracop98 <info.mario.app@gmail.com>
2026-02-13 16:44:55 +0100

    fix: use createMiddlewareSupabaseClient for older auth-helpers version

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
@@ -6,7 +6,7 @@ export async function middleware(req: NextRequest) {
     const res = NextResponse.next()
 
     // Create a Supabase client configured to use cookies
-    const supabase = createMiddlewareClient({ req, res })
+    const supabase = createMiddlewareSupabaseClient({ req, res })
 
     // Refresh session if expired - required for Server Components
     const { data: { session } } = await supabase.auth.getSession()

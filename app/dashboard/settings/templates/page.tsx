be477077fa522e56dd08f5972d7adfe9e8ae175c
fracop98 <info.mario.app@gmail.com>
2026-02-13 18:42:05 +0100

    fix: restore missing brace in templates page

--- app/dashboard/settings/templates/page.tsx
+++ app/dashboard/settings/templates/page.tsx
@@ -244,7 +244,7 @@ export default function TemplatesPage() {
                         </div>
                     </div>
                 </div >
-            )
+            )}
 
         </div >
     )
